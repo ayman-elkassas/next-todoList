2411f61258d235dc05947ddc025ff44a458eb403
ayman-elkassas <aymanelkassas88@gmail.com>
2021-03-31 13:49:24 -0700

    don

--- components/Layout/footer.js
+++ components/Layout/footer.js
@@ -7,7 +7,6 @@ function Footer(props) {
             <footer>
                 Copy Right<br/>
                 <Link href="/blog/post">Post</Link>
-                {/* test */}
             </footer>
         </div>
     );
